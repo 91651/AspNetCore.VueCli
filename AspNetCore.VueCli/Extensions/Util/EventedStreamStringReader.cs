2214ec8e94a2cd0782cc923ea76641016ffe0d62
刘泽华 <Zehua.Liu@britishcouncil.org.cn>
2018-11-28 11:56:30 +0800

    添加项目文件。

---
+++ AspNetCore.VueCli/Extensions/Util/EventedStreamStringReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using AspNetCore.VueCli.NodeServices.Util;
+
+namespace AspNetCore.VueCli.Util
+{
+    internal class EventedStreamStringReader : IDisposable
+    {
+        private EventedStreamReader _eventedStreamReader;
+        private bool _isDisposed;
+        private StringBuilder _stringBuilder = new StringBuilder();
+
+        public EventedStreamStringReader(EventedStreamReader eventedStreamReader)
+        {
+            _eventedStreamReader = eventedStreamReader
+                ?? throw new ArgumentNullException(nameof(eventedStreamReader));
+            _eventedStreamReader.OnReceivedLine += OnReceivedLine;
+        }
+
+        public string ReadAsString() => _stringBuilder.ToString();
+
+        private void OnReceivedLine(string line) => _stringBuilder.AppendLine(line);
+
+        public void Dispose()
+        {
+            if (!_isDisposed)
+            {
+                _eventedStreamReader.OnReceivedLine -= OnReceivedLine;
+                _isDisposed = true;
+            }
+        }
+    }
+}
---
+++ AspNetCore.VueCli/VueCli/VueCliMiddlewareExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.SpaServices;
+
+namespace AspNetCore.VueCli
+{
+    public static class VueCliMiddlewareExtensions
+    {
+        public static void UseVueDevelopmentServer(
+            this ISpaBuilder spaBuilder,
+            string npmScript)
+        {
+            if (spaBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(spaBuilder));
+            }
+
+            var spaOptions = spaBuilder.Options;
+
+            if (string.IsNullOrEmpty(spaOptions.SourcePath))
+            {
+                throw new InvalidOperationException($"To use {nameof(UseVueDevelopmentServer)}, you must supply a non-empty value for the {nameof(SpaOptions.SourcePath)} property of {nameof(SpaOptions)} when calling {nameof(SpaApplicationBuilderExtensions.UseSpa)}.");
+            }
+
+            VueCliMiddleware.Attach(spaBuilder, npmScript);
+        }
+    }
+}
